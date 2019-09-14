6943bd09ccae19cdacaa783b3eaff59712c17236
newpotsdev <newpotsdev@users.noreply.github.com>
2019-09-14 21:29:38 +0200

    -poop

--- purestatus.py
+++ purestatus.py
@@ -5,24 +5,16 @@ from pytz import timezone
 
 import asyncio
 import datetime
-import utils
+from modules import utils
 
-
-try:
-    from loguru import logger
-    logger.debug('Loguru found! Using fast logs...')
-except ModuleNotFoundError:
-    import logging
-    logging.basicConfig(level='DEBUG')
-    logger = logging.getLogger()
-    logger.debug('Loguru not found. Using bad slow logging...')
+import logging
+logging.basicConfig(level='INFO')
+logger = logging.getLogger()
 
 try:
     import ujson as json_parser
-    logger.debug('UJSON found! Using fast loading...')
 except ModuleNotFoundError:
     import json as json_parser
-    logger.debug('UJSON not found. Using slow loading...')
 
 
 with open('configuration.json') as fh:
@@ -55,9 +47,7 @@ async def application_cycle():
         _pattern = _pattern.replace('%emoji_time%', utils.digits_to_emojis(str(datetime.datetime.now().astimezone(timezone(config['timezone'])).strftime(config['time_pattern']))))
         await asyncio.sleep(0.2)
         if (await fetch_profile_info(application))["status"] != _pattern:
-            logger.debug("Changing status...")
             await set_status(application, _pattern)
-            logger.debug('Status changed!')
         if config['lifetime_online_settings']['enabled'] is True:
             await set_online(application)
         await asyncio.sleep(0.5)
